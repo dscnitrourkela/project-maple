0df5dbffb65de8c0c120f4638074ed88abebf001
Astha Nayak <nayakastha2911@gmail.com>
2021-06-20 19:26:57 +0530

    automatic authentication enabled

--- aptiche/lib/views/home/homescreen.dart
+++ aptiche/lib/views/home/homescreen.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('Home Screen'),
+        child: Text('log out'),
       ),
     );
   }
