e6983df4b4837d64555dce10ff90b212cee0b3fa
Astha Nayak <nayakastha2911@gmail.com>
2021-06-21 23:04:55 +0530

    minor bug fixes

--- aptiche/lib/views/home/homescreen.dart
+++ aptiche/lib/views/home/homescreen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   
+    //AuthService().signOut();
     return Scaffold(
       body: Center(
         child: Text(name),
