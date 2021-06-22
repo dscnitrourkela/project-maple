84789d9143b3a22b372d668d495f76b63403c758
Astha Nayak <nayakastha2911@gmail.com>
2021-06-22 18:25:50 +0530

    snackbar view added along with overflow error fixed

--- aptiche/lib/net/authservice.dart
+++ aptiche/lib/net/authservice.dart
@@ -1,10 +1,10 @@
+import 'package:aptiche/utils/theme.dart';
 import 'package:aptiche/views/data%20entry/dataentry.dart';
 import 'package:aptiche/views/home/homescreen.dart';
 import 'package:aptiche/views/login/loginscreen.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class AuthService extends GetxController {
@@ -27,47 +27,24 @@ class AuthService extends GetxController {
 
   void signOut() {
     FirebaseAuth.instance.signOut();
-    Get.offAll<dynamic>(() => const LoginView());
   }
 
-  void signIn(AuthCredential authCredential) async {
+  void signInwithOTP(String smsCode, String verId) async {
     try {
-      print('a1');
-      final dynamic userCredential =
-          (await FirebaseAuth.instance.signInWithCredential(authCredential))
-              .user;
-      print('a2');
-      // Get.to<dynamic>(() => const DataEntryScreen());
-      /*  if (FirebaseAuth.instance.currentUser == null) {
-        Get.snackbar<dynamic>(
-          '',
-          'Something went wrong. Please try again',
-        );
-      } else {
-        Get.to<dynamic>(() => const DataEntryScreen());
-      } */
-    } on PlatformException catch (e) {
-      print(e.toString());
-      print('a3');
-      Get.snackbar<dynamic>(
-        '',
-        'Something went wrong. Please try again',
-      );
-    }
-  }
-
-  void signInwithOTP(String smsCode, String verId) {
-    try {
-      print('b1');
       final AuthCredential authCredential = PhoneAuthProvider.credential(
         verificationId: verId,
         smsCode: smsCode,
       );
-      signIn(authCredential);
-      print('b2');
-    } catch (e) {
-      print('b3');
-      print(e);
+      await FirebaseAuth.instance.signInWithCredential(authCredential);
+
+      Get.to<dynamic>(() => const DataEntryScreen());
+    } catch (error) {
+      Get.snackbar<dynamic>('Authentication Error - WRONG OTP',
+          'Please enter the correct OTP sent to your mobile number',
+          backgroundColor: kSecondaryColor,
+          colorText: Colors.white,
+          isDismissible: true,
+          snackStyle: SnackStyle.GROUNDED);
     }
   }
 }
