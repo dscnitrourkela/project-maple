ceaed7c7a33f4e6ebab8cba81a64ff8f9bd7f819
Astha Nayak <nayakastha2911@gmail.com>
2021-06-27 02:06:37 +0530

    creation of fake-api endpoints

--- aptiche/lib/net/api.dart
+++ aptiche/lib/net/api.dart
@@ -1,3 +1,9 @@
-abstract class Api{
- 
+import 'package:aptiche/datamodels/quiz.dart';
+
+abstract class Api {
+  Future<List<Quiz>> getPastQuiz();
+
+  Future<List<Quiz>> getFutureQuiz();
+
+  Future<Quiz> getLiveQuiz();
 }
