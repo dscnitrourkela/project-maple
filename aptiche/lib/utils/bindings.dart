597eef4f52d77a43a040b05bbd708e3acfb3d644
Sriram Patibanda <patibandasriram2k02@gmail.com>
2021-06-27 23:13:38 +0530

    Add HomeController along with its binding along with UI changes to HomeScreen

--- aptiche/lib/utils/bindings.dart
+++ aptiche/lib/utils/bindings.dart
@@ -1,7 +1,7 @@
 import 'package:aptiche/net/authservice.dart';
 import 'package:aptiche/views/data%20entry/dataentry_controller.dart';
+import 'package:aptiche/views/home/home_controller.dart';
 import 'package:aptiche/views/login/logincontroller.dart';
-
 import 'package:get/instance_manager.dart';
 
 class HomeBinding implements Bindings {
@@ -10,6 +10,6 @@ class HomeBinding implements Bindings {
     Get.put<AuthService>(AuthService());
     Get.put<LoginController>(LoginController());
     Get.put<DataEntryController>(DataEntryController());
-  
+    Get.put<HomeController>(HomeController());
   }
 }
