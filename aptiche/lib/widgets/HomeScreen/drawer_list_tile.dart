085e0f8d99eec834d6ff71d44b4a2ea6a839f578
Sriram Patibanda <patibandasriram2k02@gmail.com>
2021-06-30 16:21:51 +0530

    Changes const sizes to dynamic sizes using size config

--- aptiche/lib/widgets/HomeScreen/drawer_list_tile.dart
+++ aptiche/lib/widgets/HomeScreen/drawer_list_tile.dart
@@ -1,3 +1,4 @@
+import 'package:aptiche/utils/ui_scaling.dart';
 import 'package:flutter/material.dart';
 
 class DrawerListTile extends StatelessWidget {
@@ -9,13 +10,16 @@ class DrawerListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 10.0, bottom: 15.0),
+      padding: EdgeInsets.only(
+        left: SizeConfig.safeBlockHorizontal!,
+        bottom: SizeConfig.safeBlockVertical! * 2,
+      ),
       child: ListTile(
         title: Text(
           text,
           style: Theme.of(context).primaryTextTheme.bodyText2!.copyWith(
                 fontWeight: FontWeight.bold,
-                fontSize: 20,
+                fontSize: SizeConfig.safeBlockHorizontal! * 5,
               ),
         ),
       ),
