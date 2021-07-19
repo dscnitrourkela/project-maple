c7dd77be2bf4cdf4be4bbf4d281d6044582b1ee8
Sriram Patibanda <patibandasriram2k02@gmail.com>
2021-07-19 21:50:34 +0530

    Fixes bugs with getQuizzes query -1

--- aptiche/lib/services/graphql.dart
+++ aptiche/lib/services/graphql.dart
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 import 'package:aptiche/utils/query.dart';
+=======
+import 'dart:convert';
+
+import 'package:aptiche/datamodels/api_models.dart';
+import 'package:aptiche/services/net/authservice.dart';
+>>>>>>> b6c3b76 (Fixes bugs with getQuizzes query)
 import 'package:aptiche/utils/string.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
@@ -17,6 +24,21 @@ class GraphQLService {
   }
 
   Future<void> getUser() async {
+<<<<<<< HEAD
+=======
+    const String query = r'''
+      query getUsers($ids: [ObjectId!]!){
+        getUsers(ids: $ids) {
+            name
+            email
+            rollNo
+            _id
+            uid
+            fcmToken
+          }
+        }
+    ''';
+>>>>>>> b6c3b76 (Fixes bugs with getQuizzes query)
     final QueryOptions options = QueryOptions(
         document: gql(getUsers),
         variables: <String, List<String>>{'ids': <String>[]},
@@ -55,15 +77,52 @@ class GraphQLService {
   }
 
   Future<void> getQuizzes() async {
+<<<<<<< HEAD
     final QueryOptions options = QueryOptions(
       document: gql(getQuiz),
       variables: <String, dynamic>{'ids': <String>[]},
+=======
+    const String query = r'''
+      query getQuizzes($ids: [ObjectId!]!){
+        getQuizzes(ids: $ids){
+          _id,
+          name,
+          startTime,
+          endTime,
+        }
+      }
+    ''';
+    final QueryOptions options = QueryOptions(
+      document: gql(query),
+      variables: <String, List<String>>{'ids': []},
+>>>>>>> b6c3b76 (Fixes bugs with getQuizzes query)
     );
     try {
       final QueryResult result = await _client.query(options);
       if (result.hasException) {
+<<<<<<< HEAD
         debugPrint(result.exception.toString());
       }
+=======
+        print(result.exception);
+      }
+
+      // print(result.data);
+
+      List<Object?> quizzes = result.data!['getQuizzes'] as List<Object?>;
+
+      // quizzes.forEach(Object (element) {
+      //   Quiz.fromJson(element);
+      // });
+
+      // quizzes.map<Quiz>((json) => Quiz.fromJson(json)).toList();
+
+      // final List<Quiz> quizzes = result.data! as List<Quiz>;
+
+      print(quizzes);
+
+      // return quizzes;
+>>>>>>> b6c3b76 (Fixes bugs with getQuizzes query)
     } catch (e) {
       rethrow;
     }
